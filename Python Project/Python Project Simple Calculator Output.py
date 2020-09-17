823fc9c7884d91723bdd47e07d831e5761555e8b
SyedaSaraSameen1234 <69796239+SyedaSaraSameen1234@users.noreply.github.com>
2020-09-17 12:59:09 +0530

    Add files via upload

---
+++ Python Project/Python Project Simple Calculator Output.py
@@ -0,0 +1,36 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: D:/Program Files/Python38-32/Python Project Simple Calculator.py ==
+Select opeartion
+1.Add
+2.Subtract
+3.Multiply
+4.Divide
+Enter choice(1/2/3/4):1
+Enter choice(1/2/3/4):
+=== RESTART: D:/Program Files/Python38-32/Python Project Simple Calculator.py ==
+Select opeartion
+1.Add
+2.Subtract
+3.Multiply
+4.Divide
+Enter choice(1/2/3/4):1
+Enter first number:7
+Enter second number:8
+7.0 + 8.0 = 15.0
+Enter choice(1/2/3/4):2
+Enter first number:7
+Enter second number:0
+7.0 - 0.0 = 7.0
+Enter choice(1/2/3/4):3
+Enter first number:9
+Enter second number:7
+9.0 * 7.0 = 63.0
+Enter choice(1/2/3/4):4
+Enter first number:87
+Enter second number:76
+87.0 / 76.0 = 1.144736842105263
+Enter choice(1/2/3/4):8
+Invalid Input
+Enter choice(1/2/3/4):
